ccdcb8fdc88112952447a9203bd51a04053b0649
qunax <procop4uck.ilia@gmail.com>
2025-03-18 01:22:42 +0100

    UPDATE: .net8

--- README.md
+++
@@ -1,8 +0,0 @@
-# TNoodle.NET
-A C# portable library implementation of the official WCA tnoodle project.
-
-## Performance
-This really runs faster than the original Java implementation.
-
-## Known Issues
-The SQ1 scrambler is not working properly. 
---
+++ TestApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using TNoodle.Puzzles;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var r = new Random(2017);
+        var watch = new Stopwatch();
+        var tick = 0.0;
+        const int count = 50;
+
+        var puzzle = new ClockPuzzle();
+
+        for (var i = 0; i < count; i++)
+        {
+            watch.Restart();
+
+            var result = puzzle.GenerateWcaScramble(r);
+
+            watch.Stop();
+            Console.WriteLine(result);
+            tick += watch.ElapsedTicks;
+        }
+
+        tick /= count;
+        Console.WriteLine($"{tick / TimeSpan.TicksPerMillisecond} ms");
+    }
+}
